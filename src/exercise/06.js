2ccad99f0be6e941fc2d9cd99e69340dfb825eff
Abhilash Nair <abhilashnair@MacBook-Pro.domain>
2023-04-14 11:14:02 -0600

    get event values for event handlers

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,14 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
+  function handleSubmit(event) {
+    event.preventDefault()
+    // console.dir(event.target)
+    // can also use id, name, or index in target
+    const value = event.target.elements.usernameInput.value
+    onSubmitUsername(value)
+  }
+
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -20,10 +28,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input id="usernameInput" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
